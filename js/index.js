e6e44eef6a40c4597c636b93991f14f9487a3053
cjrt27 <cameron.jr.thorn@gmail.com>
2018-04-26 21:35:58 +0100

    changes to rendering and adding if conditions to js

--- js/index.js
+++ js/index.js
@@ -19,7 +19,7 @@ $.ajax({
 
 
   $.each(rawDataNyt.results, function(key, value) {
-    var img= value.multimedia[1].url
+    var img= value.multimedia[4].url
     var abstract= value.abstract
     var url= value.url
   
@@ -45,4 +45,29 @@ return key<11;
 });
 
 })
+
+
+$("select").on("change", function(event) {
+if(screen.width <= "599") { 
+$(".header").css({"margin-top":"1rem","max-height":"200px"})  
+$("#logo").css({"text-align":"center"})
+$(".header img").css({"height": "50%","width":"50%", "margin":"auto"})
+$("footer").css({});
+}
+else if(screen.width >="600" && screen.width <= "1239") {
+    $(".header").css({"margin-top":"0rem","max-height":"200px", "margin-right":"20rem"})  
+    $("#logo").css({"text-align":"center", "margin-right":"3rem", "max-width":"134px"})
+    $(".header img").css({"height": "50%","width":"50%", "margin":"auto"})
+    $("footer").css({"margin-top":"3rem"});
+}
+else if(screen.width >="1240" && screen.width <= "8000") {
+  $(".header").css({"margin-top":"0rem","max-height":"200px", "margin-right":"0rem"})  
+  $("#logo").css({"margin-right":"0rem", "padding-bottom": "0rem", "max-width":"70px","max-height":"70px"})
+  $(".header img").css({"height": "100%","width":"100%", "margin":"0rem"})
+  $("footer").css({"margin-top":"3rem"});
+  $(".selectwords").css({"margin-left":"3rem"});
+}
+
+})
+
 })
