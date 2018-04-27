c2944bf74651c0e7c705d5d2b8d04b0a0ab214f3
cjrt27 <cameron.jr.thorn@gmail.com>
2018-04-27 21:02:23 +0100

    fixing header auto on change

--- js/index.js
+++ js/index.js
@@ -1,9 +1,15 @@
 $(function() {
- 
+
+  
+
+
  var rawDataNyt =""
 
 $("select").on('change', function() {
-   var sections = $('select[name=selector]').val()
+ // add empty container here to the div that you add the below to gif function to
+ // add function here which creates div which you then put gif into and then use animate to get it to run 
+ 
+  var sections = $('select[name=selector]').val()
     var url = "https://api.nytimes.com/svc/topstories/v2/" + sections + ".json";
 url += '?' + $.param({
   'api-key': "69cbc8f4739f440b86f94a43bdd196a4"
@@ -41,19 +47,19 @@ $.ajax({
 
 $("select").on("change", function(event) {
 if(screen.width <= "599") { 
-$(".header").css({"max-height":"210px"})  
+$(".header").css({"height":"auto"})  
 $("#logo").css({"text-align":"center"})
 $(".header img").css({"height": "50%","width":"50%", "margin":"auto"})
 $("footer").css({"margin-top":"20px"});
 }
 else if(screen.width >="600" && screen.width <= "1239") {
-    $(".header").css({"width":"535px","margin-top":"0rem","max-height":"70px","display":"flex","justify-content":"flex-start"})  
+    $(".header").css({"height":"auto","display":"flex","justify-content":"flex-start"})  
     $("#logo").css({"text-align":"center", "margin-right":"3rem", "max-width":"134px"})
     $(".header img").css({"height": "50%","width":"50%", "margin":"auto"})
     $("footer").css({"margin-top":"3rem"});
 }
 else if(screen.width >="1240" && screen.width <= "8000") {
-  $(".header").css({"margin-top":"0rem","max-height":"70px", "margin-right":"0rem", "padding":"2rem"})  
+  $(".header").css({"height":"auto"})  
   $("#logo").css({"margin-right":"0rem", "padding-bottom": "0rem", "max-width":"70px","max-height":"70px"})
   $(".header img").css({"height": "100%","width":"100%", "margin":"0rem"})
   $("footer").css({"margin-top":"3rem"});
